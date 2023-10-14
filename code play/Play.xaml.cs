d439a1cf55809a02cad6e7b7b9929e656ffff455
JeickobI <72135997+JeickobI@users.noreply.github.com>
2023-10-14 15:39:00 +0400

    Add files via upload

--- code play/Play.xaml.cs
+++ code play/Play.xaml.cs
@@ -11,12 +11,54 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace code_play
 {
     /// <summary>
     /// Логика взаимодействия для Play.xaml
     /// </summary>
+    /// 
+    class Programm
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["regBD"].ConnectionString;
+
+        private static SqlConnection sqlConnection = null;
+
+        static void Main(string[] args, SqlConnection sqlConnection)
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            SqlDataReader sqlDataReader = null;
+
+            string command = string.Empty;
+
+            while (true)
+            {
+                command = Console.ReadLine();
+
+                if (command.ToLower().Equals("Выход"))
+                {
+                    if (sqlConnection.State == ConnectionState.Open)
+                    {
+                        sqlConnection.Close();
+                    }
+
+                    if (sqlDataReader != null)
+                    {
+                        sqlDataReader.Close();
+                    }
+
+                    break;
+                }               
+            }
+            Console.WriteLine("Для продолжения нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
     public partial class Play : Window
     {
         public Play()
@@ -24,5 +66,19 @@ namespace code_play
             InitializeComponent();
         }
 
+        private void vihod1_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void lideri_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Лидеры: \n");
+        }
+
+        private void reg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
